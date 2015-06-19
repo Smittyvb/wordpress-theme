c3309288117c9ccc40f59bce6f3287a431266ae6
Suzanne <suzannezc@users.noreply.github.com>
2015-06-19 13:39:44 -0400

    Adding function calls instead of code

--- index.php
+++ index.php
@@ -1,109 +1,19 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<div class="container">
-  <div class="row">
+wrdsb_sidebar_start();
 
-    <?php $has_left = FALSE; ?>
-    <?php $has_right = FALSE; ?>
-    <?php if (is_active_sidebar('sidebar-left') || has_nav_menu('left')) {$has_left = TRUE;} ?>
-    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')) {$has_right = TRUE;} ?>
-    <?php if (!is_front_page()) {$has_left = TRUE;} ?>
+// Start the Loop.
+while ( have_posts() ) : the_post();
 
-    <?php
-    # Both sidebars
-    # left column
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-      if (!is_front_page()) {
-        get_sidebar('lmenu');
-      }
-      get_sidebar('left');
-      echo '</div>';
+  // Include the post format-specific content template.
+  get_template_part( 'content', get_post_format() );
 
-    # Just left sidebar
-    elseif (($has_left == TRUE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-3 col-lg-3">';
-      if (!is_front_page()) {
-        get_sidebar('lmenu');
-      }
-      get_sidebar('left');
-      echo '</div>';
+  // If comments are open or we have at least one comment, load up the comment template.
+  if ( comments_open() || get_comments_number() ) {
+    comments_template();
+  }
+endwhile;
 
-    # Just right sidebar
-      # Nothing to do
+wrdsb_sidebar_end();
 
-    # No sidebars
-      # Nothing to do
-    endif
-    ?>
-
-    <?php
-    # Both sidebars
-    # content area
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-6 col-md-8 col-lg-8">';
-
-    # Just left sidebar
-    elseif (($has_left == TRUE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-9 col-lg-9">';
-
-    # Just right sidebar
-    elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-8">';
-
-    # No sidebars
-    elseif (($has_left == FALSE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-12 col-lg-12">';
-
-    endif
-    ?>
-
-    <?php
-      // Start the Loop.
-      while ( have_posts() ) : the_post();
-      
-        // Include the post format-specific content template.
-        get_template_part( 'content', get_post_format() );
-
-        // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) {
-          comments_template();
-        }
-      endwhile;
-    ?>
-
-    </div> <!-- end content area -->
-
-    <?php
-    # Both sidebars
-    # right column
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-      if (!is_front_page()) {
-        get_sidebar('rmenu');
-      }
-      get_sidebar('right');
-      echo '</div>';
-
-    # Just left sidebar
-      # Nothing to do
-
-    # Just right sidebar
-    elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-4">';
-      if (!is_front_page()) {
-        get_sidebar('rmenu');
-      }
-      get_sidebar('right');
-      echo '</div>';
-
-    # No sidebars
-      # Nothing to do
-
-    endif
-    ?>
-
-  </div>
-</div>
-
-<?php get_footer(); ?>
+get_footer(); ?>
