101ae6284bc43098f618c0381cbbe7377371a4d5
Suzanne <suzannezc@users.noreply.github.com>
2015-06-19 12:11:54 -0400

    Update functions-posts.php

--- functions/functions-posts.php
+++ functions/functions-posts.php
@@ -33,6 +33,12 @@ function display_cats_tags()
 	global $post;
 	$number_of_cats = count(get_the_category());
 	$number_of_tags = count(get_the_tags());
+	$posttags = get_the_tags();
+	if ($posttags) {
+		foreach($posttags as $tag) {
+			$allthetags = $tag->name . ' '; 
+		}
+	}
 
 	if ($number_of_cats > 0 ) 
 	{
@@ -48,7 +54,7 @@ function display_cats_tags()
 	{
 		$cats_n_tags = '<div class="clearfix"></div>';
 		$cats_n_tags .= '<p class="categories gray-dark small">Tags: ';
-		$cats_n_tags .= get_the_tags_list('',' &bull; ','');
+		$cats_n_tags .= $allthetags;
 		$cats_n_tags .= '</p>';
 		echo $cats_n_tags;
 	}
@@ -66,7 +72,7 @@ function display_cats_tags()
 		$cats_n_tags .= '<p class="categories gray-dark small">Categories: ';
 		$cats_n_tags .= get_the_category_list(' &bull; ');
 		$cats_n_tags .= ' Tags: ';
-		$cats_n_tags .= get_the_tags_list('',' &bull; ','');
+		$cats_n_tags .= $allthetags;
 		$cats_n_tags .= '</p>';
 		echo $cats_n_tags;
 	}
