72d61273df9a1ca29d4217121c258063465b6a52
Suzanne <suzannezc@users.noreply.github.com>
2015-09-24 13:39:50 -0400

    Fixing column widths

--- 404.php
+++ 404.php
@@ -3,38 +3,70 @@
 <div class="container">
   <div class="row">
 
-    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')): ?>
-      <div class="col-sm-3 col-md-2 col-lg-2">
-    <?php else: ?>
-      <div class="col-sm-3 col-lg-2">
-    <?php endif ?>
+    <?php $has_left = FALSE; ?>
+    <?php $has_right = FALSE; ?>
+    <?php if (is_active_sidebar('sidebar-left') || has_nav_menu('left')) {$has_left = TRUE;} ?>
+    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')) {$has_right = TRUE;} ?>
 
-      <?php get_sidebar('lmenu'); ?>
-      <?php get_sidebar('left'); ?>
+    <?php
+    # Both sidebars
+    if (($has_left == TRUE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
+      get_sidebar('left');
+      echo '</div>';
+    # Just left sidebar
+    elseif (($has_left == TRUE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-3 col-lg-2">';
+      get_sidebar('left');
+      echo '</div>';
+    # Just right sidebar
+      # Nothing to do
+    # No sidebars
+      # Nothing to do
+    endif
+    ?>
 
-    </div> <!-- end sidebar div -->
-
-    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')): ?>
-      <div class="col-sm-6 col-md-7 col-lg-8">
-    <?php else: ?>
-      <div class="col-sm-9 col-lg-10">
-    <?php endif ?>
-
+    <?php
+    # Both sidebars
+    if (($has_left == TRUE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-6 col-md-8 col-lg-8">';
+    # Just left sidebar
+    elseif (($has_left == TRUE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-9 col-lg-10">';
+    # Just right sidebar
+    elseif (($has_left == FALSE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-9">';
+    # No sidebars
+    elseif (($has_left == FALSE) and ($has_right == FALSE)):
+      echo '<div class="col-sm-12 col-lg-12">';
+    endif
+    ?>
+    
       <h1>404 - Not found</h1>
       <p>Sorry, we can't find that. Maybe you'd like to try a search.</p>
       <?php get_search_form(); ?>
 
     </div> <!-- end content area -->
 
-    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')): ?>
-      <div class="col-sm-3 col-md-3 col-lg-2">
-        <?php get_sidebar('rmenu'); ?>
-        <?php get_sidebar('right'); ?>
-      </div>
-    <?php endif ?>
-
+    <?php
+    # Both sidebars
+    if (($has_left == TRUE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
+      get_sidebar('right');
+      echo '</div>';
+    # Just left sidebar
+      # Nothing to do
+    # Just right sidebar
+    elseif (($has_left == FALSE) and ($has_right == TRUE)):
+      echo '<div class="col-sm-3"">';
+      get_sidebar('right');
+      echo '</div>';
+    # No sidebars
+      # Nothing to do
+    endif
+    ?>
 
   </div>
 </div>
 
-<?php get_footer(); ?>
+<?php get_footer();
